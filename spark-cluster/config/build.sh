1e1e8ffd2ba4fb2992f6b03ecad7997f53b7cca5
Adam <adam@brightshadow.org>
2019-05-25 19:01:03 -0400

    apline build works, same size

--- spark-cluster/config/build.sh
+++ spark-cluster/config/build.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+docker system prune -f
+
 set -e
 
 SPARK_VERSION="$(curl -s http://mirror.synyx.de/apache/spark/ | grep -oP 'spark-\K(\d\.\d\.\d)(?=/)' |  sort --version-sort | tail -1)"
 
-echo $SPARK_VERSION
+echo "SPARK version : $SPARK_VERSION"
 
 HADOOP_VERSION="$(curl -s http://apache.lauf-forum.at/hadoop/common/ | grep -oP '(\d+\.\d+\.\d+)(?=/)' | sort --version-sort | tail -1)"
 
-echo $HADOOP_VERSION
+echo "HADOOP version: $HADOOP_VERSION"
+
+SCALA_VERSION="$(curl -s https://www.scala-lang.org/blog/announcements/ | grep -oP '(?<=Scala )(\d+\.\d+\.\d+)(?= is.*)' | head -1)"
+
+echo "SCALA version: $SCALA_VERSION"
+
+SBT_VERSION="$(curl -s https://github.com/sbt/sbt/releases | grep -oP "(?<=v)(\d+\.\d+\.\d+)(?=\""")" | uniq | head -1)"
+
+echo "SBT version: $SBT_VERSION"
 
 if [ ! -f config/id_rsa ]; then
     sh config/genkey.sh
@@ -18,11 +28,15 @@ if [ -f build.out ]; then
     rm build.out
 fi
 
-docker build -t spark-base \
-    --build-arg SPARK_VERSION=$SPARK_VERSION \
-    --build-arg HADOOP_VERSION=$HADOOP_VERSION . \
-    | while read line; do echo "$(date +%s) $line"; done \
-     > build.out 2>&1 &
+start_time=$(date +%s)
 
-timeout 5 tail -f build.out
+docker build -f $1 -t spark-base \
+    --build-arg SPARK_VERSION=$SPARK_VERSION \
+    --build-arg HADOOP_VERSION=$HADOOP_VERSION \
+    --build-arg SCALA_VERSION=$SCALA_VERSION \
+    --build-arg SBT_VERSION=$SBT_VERSION . \
+    | while read line; do echo "$((`date +%s` - $start_time)) $line"; done \
+     > build.out 2>&1 | tee build.out &
+
+cat build.out
 # (timeout 5 cat build.out || : ) | tail
